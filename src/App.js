b31cabaa916b690da99ef7c59c8fae411e6b6d13
aishadabbagh <aisha.dabbagh@gmail.com>
2019-10-27 19:04:57 +0300

    homework w05d01

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
+
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Movie from './Movies.js';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div class = "App" >
+        
+        <Movie title="The Fellowship of the Ring" hours="2" minutes="58"  //source={"https://m.media-amazon.com/images/M/MV5BN2EyZjM3NzUtNWUzMi00MTgxLWI0NTctMzY4M2VlOTdjZWRiXkEyXkFqcGdeQXVyNDUzOTQ5MjY@._V1_.jpg"} 
+        ></Movie>
+        <Movie title="The Two Towers" hours="2" minutes="59" //source={"https://upload.wikimedia.org/wikipedia/en/a/ad/Lord_of_the_Rings_-_The_Two_Towers.jpg"} 
+        ></Movie>
+        <Movie title="The Return of the King" hours="3" minutes="21" //source={"https://upload.wikimedia.org/wikipedia/en/9/9d/Lord_of_the_Rings_-_The_Return_of_the_King.jpg"}
+        ></Movie>
+      
+      </div>
+    );
+  }
 }
 
 export default App;
+
